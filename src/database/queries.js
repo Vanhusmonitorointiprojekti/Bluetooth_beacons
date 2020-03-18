25fd8ca50ca06d4cece455c3034b975ccbd1f905
Marski96 <jani.maaranen@saunalahti.fi>
2020-03-18 11:43:49 +0200

    fixed error in beacon_locations

--- src/database/queries.js
+++ src/database/queries.js
@@ -24,6 +24,14 @@ global.GET_last_beacon_detections =
             ORDER BY measument_time DESC \
             LIMIT 10 \
             ';
+
+global.GET_beacon_locations =
+            ' \
+            (SELECT d.receiver_id, i.beacon_user, d.signal_db, d.measument_time FROM beacon_detections d \
+            JOIN beacon_info i ON (d.beacon_id = i.beacon_id) WHERE d.beacon_id = "e2:e3:23:d1:b0:54" ORDER BY measument_time DESC limit 1) \
+            UNION (SELECT d.receiver_id, i.beacon_user, d.signal_db, d.measument_time FROM beacon_detections d JOIN beacon_info i ON (d.beacon_id = i.beacon_id) WHERE d.beacon_id = "d6:2c:ca:c0:d4:9c" ORDER BY measument_time DESC limit 1) \
+            UNION (SELECT d.receiver_id, i.beacon_user, d.signal_db, d.measument_time FROM beacon_detections d JOIN beacon_info i ON (d.beacon_id = i.beacon_id) WHERE d.beacon_id = "f2:36:00:21:c0:50" ORDER BY measument_time DESC limit 1); \
+            ';
             
 global.GET_detections_ranneke1 = 
             ' \
