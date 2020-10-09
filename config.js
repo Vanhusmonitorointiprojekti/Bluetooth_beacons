33b56b496f75fb052cca8095a676e493b11da113
M. Hyvarinen <54714032+marhyvar@users.noreply.github.com>
2020-10-09 17:02:10 +0300

    Add schema, connection to nrt_db and add mqtt client for nrt

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
+require('dotenv').config()
+
 module.exports = {
     database: {
       db: process.env.RDB_DB || "rt_beacons",
       host: process.env.RDB_HOST || "localhost",
       port: process.env.RDB_PORT || 28015
-    },
-  
-    port: process.env.APP_PORT || 3000
+    }
   }
---
+++ src/non_realtime_db/mqtt_client.js
@@ -0,0 +1,49 @@
+const mqtt = require('mqtt')
+const mysql = require('mysql');
+const client  = mqtt.connect('mqtt://195.148.21.81')
+const db = require('./nrt_db')
+
+client.on('connect', function () {
+  client.subscribe('Beacon-detect', function (err) {
+    if (!err) {
+      console.log('connected!')
+    }
+  })
+})
+    
+client.on('message', function (topic, message) {
+  // message is Buffer, hence make it toString()
+  let msgArray = message.toString().split(" ")
+  //console.log(message.toString())
+  writeValues(msgArray)
+})
+
+function writeValues(msg) {
+  const receiver_id = msg[0]
+  const beacon_id = msg[1]
+  const signal_db = msg[2]
+
+  let sql = 'INSERT INTO ?? (??, ??, ??) VALUES (?, ?, ?)'
+  const params = ['beacon_detections', 'receiver_id', 'beacon_id', 'signal_db',
+    receiver_id, beacon_id, signal_db]
+  sql = mysql.format(sql, params)
+  db.query(sql, function(error, results) {
+      if (error) {
+          console.log(error)
+          return
+      }
+  })
+
+
+  /*r.db('rt_beacons').table('beacon_detections').insert( {
+    receiver_id: msg[0],
+    beacon_id: msg[1],
+    signal_db: msg[2],
+    measurement_time: Date.now()
+  }).run(connection, function(err, result) {
+    if (err) throw err;
+    console.log(JSON.stringify(result, null, 2))
+  })*/
+}
+
+//https://gist.github.com/smching/ff414e868e80a6ee2fbc8261f8aebb8f#file-app_mqtt_mysql_completed-js
